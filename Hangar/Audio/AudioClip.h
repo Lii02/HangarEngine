6de1b11000daf2aa293d747b68dd99c0786df486
Lii02 <lukeinlow@gmail.com>
2022-08-15 22:29:38 -0400

    AudioClip, AudioEngine deleted copy constructor

--- Hangar/Audio/AudioClip.h
+++ Hangar/Audio/AudioClip.h
@@ -8,6 +8,7 @@ private:
 public:
 	AudioClip();
 	~AudioClip();
+	AudioClip(const AudioClip&) = delete;
 
 	void SetData(void* data, uint32_t format, uint32_t size, uint32_t frequency);
 	unsigned int GetID() const;
--- Hangar/Audio/AudioEngine.h
+++ Hangar/Audio/AudioEngine.h
@@ -14,6 +14,7 @@ public:
 
 	AudioEngine();
 	~AudioEngine();
+	AudioEngine(const AudioEngine&) = delete;
 
 	void Initialize();
 	void DeInitialize();
